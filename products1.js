af2beddc3008cef5ef89fd7ec4ee77812dc0727d
skdhameliya <53290891+skdhameliya@users.noreply.github.com>
2020-07-14 13:32:00 +0530

    skd1

---
+++ products1.js
@@ -0,0 +1,98 @@
+var myProduct = {
+    // "name":"skd",
+    // "num":100,
+    "pZone": [
+        {
+            "id":"0",
+            "photos":["./img/kainat_1.jpeg", "./img/kainat_2.jpeg", "./img/kainat_3.jpeg", "./img/kainat_4.jpeg", "./img/kainat_5.jpeg", "./img/kainat_6.jpeg"], 
+            "catalog" : "Kainat",
+            "top" : "NET (with heavy Handwork)",
+            "bottom" : "Heavy Santoon( 2.0 mtr)",
+            "inner" : "Heavy Santoon(2.0 mtr)",
+            "dupatta" : "NET with for side border + embroidery work",
+            "sleeve" : "Full",
+            // "size" : "Semistichhed",
+            "price": "1099"
+        },
+        {
+            "id":"1",
+            "photos":["./img/Mohini_71002_1.jpeg", "./img/Mohini_71002_2.jpeg", "./img/Mohini_71002_3.jpeg"], 
+            "catalog" : "Mohini 71002",
+            "top" : "Net With Embroidery work",
+            "bottom" : "Santoon",
+            "inner" : "Santoon",
+            "dupatta" : "Net",
+            "sleeve" : "Full",
+            "price": "1099"
+        },
+        {
+            "id":"2",
+            "photos":["./img/LT_Nitya_2801_1.jpeg", "./img/LT_Nitya_2801_2.jpeg", "./img/LT_Nitya_2801_3.jpeg"], 
+            "catalog" : "LT Nitya 2801",
+            "top" : "Heavy faux with inner join",
+            "bottom" : "Santoon",
+            "inner" : "Santoon",
+            "dupatta" : "Heavy net with embroidery work",
+            "sleeve" : "Full",
+            "price": "1149"
+        },
+        {
+            "id":"3",
+            "photos":["./img/LT_2202_1.jpeg", "./img/LT_2202_2.jpeg"], 
+            "catalog" : "LT 2202",
+            "top" : "Heavy faux with inner join",
+            "bottom" : "Santoon",
+            "inner" : "Santoon",
+            "dupatta" : "Heavy faux georgette with 4 side border less",
+            "sleeve" : "Full",
+            "price": "1099"
+        },
+        {
+            "id":"4",
+            "photos":["./img/Maisha_5603_1.jpeg", "./img/Maisha_5603_2.jpeg"], 
+            "catalog" : "Maisha 5603",
+            "top" : "Net",
+            "bottom" : "Santoon",
+            "inner" : "Santoon",
+            "dupatta" : "Nazneen",
+            "sleeve" : "Full",
+            "price": "899"
+        },
+        {
+            "id":"5",
+            "photos":["./img/Rang_Rasiya_UE04_1.jpeg", "./img/Rang_Rasiya_UE04_2.jpeg", "./img/Rang_Rasiya_UE04_3.jpeg"], 
+            "catalog" : "Rang Rasiya UE04",
+            "top" : "NET (with heavy Handwork)",
+            "bottom" : "Heavy Santoon( 2.0 mtr)",
+            "inner" : "Heavy Santoon(2.0 mtr)",
+            "dupatta" : "NET with for side border + embroidery work",
+            "sleeve" : "Full",
+            "price": "1049"
+        },
+        {
+            "id":"6",
+            "photos":["./img/LT_Nitya_2806_1.jpeg", "./img/LT_Nitya_2806_2.jpeg", "./img/LT_Nitya_2806_3.jpeg", "./img/LT_Nitya_2806_4.jpeg", "./img/LT_Nitya_2806_5.jpeg", "./img/LT_Nitya_2806_6.jpeg", "./img/LT_Nitya_2806_7.jpeg", "./img/LT_Nitya_2806_8.jpeg", "./img/LT_Nitya_2806_9.jpeg", "./img/LT_Nitya_2806_10.jpeg"], 
+            "catalog" : "LT Nitya 2806",
+            "top" : "Heavy faux with inner join",
+            "bottom" : "Santoon",
+            "inner" : "Santoon",
+            "dupatta" : "Nazneen",
+            "sleeve" : "full",
+            "price": "1049"
+        },
+        {
+            "id":"7",
+            "photos":["./img/Mohini_73001_1.jpeg", "./img/Mohini_73001_2.jpeg", "./img/Mohini_73001_3.jpeg"], 
+            "catalog" : "Mohini 73001",
+            "top" : "Heavy Butterfly Net with Heavy Cording work + Sikvans",
+            "bottom" : "Heavy Santoon",
+            "inner" : "Heavy Santoon",
+            "dupatta" : "Heavy Butterfly Net with 4 side border less",
+            "sleeve" : "full sleeves with hesvy cording work + sikvans",
+            "price": "1149"
+        }
+
+        
+    
+    ]
+};

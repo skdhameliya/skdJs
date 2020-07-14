556361e921a791282d38b8af4092142cc3dacb02
skdhameliya <53290891+skdhameliya@users.noreply.github.com>
2020-07-14 13:40:39 +0530

    skd_14_07_2020_01_40_PM

--- products1.js
+++ products1.js
@@ -90,6 +90,28 @@ var myProduct = {
             "dupatta" : "Heavy Butterfly Net with 4 side border less",
             "sleeve" : "full sleeves with hesvy cording work + sikvans",
             "price": "1149"
+        },
+        {
+            "id":"8",
+            "photos":["./img/LT_Nitya_2806_1.jpeg", "./img/LT_Nitya_2806_2.jpeg", "./img/LT_Nitya_2806_3.jpeg", "./img/LT_Nitya_2806_4.jpeg", "./img/LT_Nitya_2806_5.jpeg", "./img/LT_Nitya_2806_6.jpeg", "./img/LT_Nitya_2806_7.jpeg", "./img/LT_Nitya_2806_8.jpeg", "./img/LT_Nitya_2806_9.jpeg", "./img/LT_Nitya_2806_10.jpeg"], 
+            "catalog" : "8LT Nitya 2806",
+            "top" : "Heavy faux with inner join",
+            "bottom" : "Santoon",
+            "inner" : "Santoon",
+            "dupatta" : "Nazneen",
+            "sleeve" : "full",
+            "price": "1049"
+        },
+        {
+            "id":"9",
+            "photos":["./img/Mohini_73001_1.jpeg", "./img/Mohini_73001_2.jpeg", "./img/Mohini_73001_3.jpeg"], 
+            "catalog" : "9Mohini 73001",
+            "top" : "Heavy Butterfly Net with Heavy Cording work + Sikvans",
+            "bottom" : "Heavy Santoon",
+            "inner" : "Heavy Santoon",
+            "dupatta" : "Heavy Butterfly Net with 4 side border less",
+            "sleeve" : "full sleeves with hesvy cording work + sikvans",
+            "price": "1149"
         }
 
         

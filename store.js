f14441d2807a13af5bcc9b32bf9a1e711f571aee
skdhameliya <53290891+skdhameliya@users.noreply.github.com>
2021-05-24 15:54:05 +0530

    Update store.js

--- store.js
+++ store.js
@@ -2,37 +2,31 @@
    {
       "courseName":"Fork CPP",
       "courseimg":"https://media.geeksforgeeks.org/img-practice/banner/fork-cpp-thumbnail.png",
-      "courseMode":"https://vegamovies.click/download-radhe-2021-web-dl-hindi-dd5-1-480p-300mb-720p-1-2gb-1080p-2gb/",
-      "courseTracks":"6 Tracks"
+      "courseMode":"https://vegamovies.click/download-radhe-2021-web-dl-hindi-dd5-1-480p-300mb-720p-1-2gb-1080p-2gb/"
    },
    {
       "courseName":"Linux & Shell Scripting Foundation",
       "courseimg":"https://media.geeksforgeeks.org/img-practice/banner/linux-shell-scripting-thumbnail.png",
-      "courseMode":"https://vegamovies.click/download-18-salahkaar-charmsukh-2021-season-1-hindi-ullu-originals-web-series-480p-720p-hdrip-2/",
-      "courseTracks":"8 Tracks"
+      "courseMode":"https://vegamovies.click/download-18-salahkaar-charmsukh-2021-season-1-hindi-ullu-originals-web-series-480p-720p-hdrip-2/"
    },
    {
       "courseName":"11 Weeks Workshop on Data Structures and Algorithms",
       "courseimg":"https://media.geeksforgeeks.org/img-practice/banner/Workshop-DSA-thumbnail.png",
-      "courseMode":"https://vegamovies.click/download-roberrt-2021-full-movie-hindi-hq-voice-over-dubbed-480p-550mb-720p-1-5gb-1080p-3gb-2/",
-      "courseTracks":"47 Tracks"
+      "courseMode":"https://vegamovies.click/download-roberrt-2021-full-movie-hindi-hq-voice-over-dubbed-480p-550mb-720p-1-5gb-1080p-3gb-2/"
    },
    {
       "courseName":"Data Structures and Algorithms",
       "courseimg":"https://media.geeksforgeeks.org/img-practice/banner/dsa-self-paced-thumbnail.png",
-      "courseMode":"https://vegamovies.click/download-radhe-2021-web-dl-hindi-dd5-1-480p-300mb-720p-1-2gb-1080p-2gb/",
-      "courseTracks":"24 Tracks"
+      "courseMode":"https://vegamovies.click/download-radhe-2021-web-dl-hindi-dd5-1-480p-300mb-720p-1-2gb-1080p-2gb/"
    },
    {
       "courseName":"11 Weeks Workshop on Data Structures and Algorithms",
       "courseimg":"https://media.geeksforgeeks.org/img-practice/banner/Workshop-DSA-thumbnail.png",
-      "courseMode":"https://vegamovies.click/download-roberrt-2021-full-movie-hindi-hq-voice-over-dubbed-480p-550mb-720p-1-5gb-1080p-3gb-2/",
-      "courseTracks":"47 Tracks"
+      "courseMode":"https://vegamovies.click/download-roberrt-2021-full-movie-hindi-hq-voice-over-dubbed-480p-550mb-720p-1-5gb-1080p-3gb-2/"
    },   
    {
       "courseName":"11 Weeks Workshop on Data Structures and Algorithms",
       "courseimg":"https://media.geeksforgeeks.org/img-practice/banner/Workshop-DSA-thumbnail.png",
-      "courseMode":"https://vegamovies.click/download-roberrt-2021-full-movie-hindi-hq-voice-over-dubbed-480p-550mb-720p-1-5gb-1080p-3gb-2/",
-      "courseTracks":"47 Tracks"
+      "courseMode":"https://vegamovies.click/download-roberrt-2021-full-movie-hindi-hq-voice-over-dubbed-480p-550mb-720p-1-5gb-1080p-3gb-2/"
    }
 ]

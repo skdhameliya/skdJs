07dfe0a50f08f257f9d62e666ec8bb79436d092c
skdhameliya <53290891+skdhameliya@users.noreply.github.com>
2021-05-26 14:57:43 +0530

    Update store.js

--- store.js
+++ store.js
@@ -1,44 +1,35 @@
 [
+   
    {
-      "courseName":"Radhe",
-      "courseimg":"https://vegamovies.co.in/wp-content/uploads/2021/05/Radhe.jpg",
-      "courseMode":"https://vegamovies.co.in/download-radhe-2021-movie-480p-720p-1080p/"
+      "courseName":"",
+      "courseimg":"",
+      "courseMode":""
    },
    {
-      "courseName":"The Big Bull",
-      "courseimg":"https://vegamovies.co.in/wp-content/uploads/2021/04/The-Big-Bull-2021.jpg",
-      "courseMode":"https://vegamovies.co.in/the-big-bull-2021-hdrip/"
+      "courseName":"",
+      "courseimg":"",
+      "courseMode":""
    },
    {
-      "courseName":"Vakeel Saab",
-      "courseimg":"https://vegamovies.co.in/wp-content/uploads/2021/04/Vakeel-Saab-2021-Hindi-poster.jpg",
-      "courseMode":"https://vegamovies.co.in/download-vakeel-saab-2021-hindi-dubbed-480p-720p-1080p/"
+      "courseName":"",
+      "courseimg":"",
+      "courseMode":""
    },
    {
-      "courseName":"Raya and the Last Dragon",
-      "courseimg":"https://vegamovies.co.in/wp-content/uploads/2021/03/Raya-and-the-Last-Dragon-Hindi-Voice.jpg",
-      "courseMode":"https://vegamovies.co.in/raya-and-the-last-dragon-2021/"
+      "courseName":"",
+      "courseimg":"",
+      "courseMode":""
    },
    {
-      "courseName":"Mortal Kombat",
-      "courseimg":"https://vegamovies.co.in/wp-content/uploads/2021/05/Mortal-Kombat-Hindi-Dubbed-poster.jpg",
-      "courseMode":"https://vegamovies.co.in/download-mortal-kombat-2021-hindi-dubbed-org-bluray-480p-720p-1080p/"
+      "courseName":"",
+      "courseimg":"",
+      "courseMode":""
    },
    {
-      "courseName":"Nobody 2021",
-      "courseimg":"https://vegamovies.co.in/wp-content/uploads/2021/04/Nobody-2021-Hindi.png",
-      "courseMode":"https://vegamovies.co.in/download-nobody-2021-dual-audio-hindi-480p-720p/"
-   },
-   {
-      "courseName":"Godzilla vs Kong 2021",
-      "courseimg":"https://vegamovies.co.in/wp-content/uploads/2021/05/Godzilla-Vs.-Kong-2021-Hindi-Dubbed.jpg",
-      "courseMode":"https://vegamovies.co.in/godzilla-vs-kong-2021-dual-audio-hindi-480p-720p-1080p-download/"
+      "courseName":"",
+      "courseimg":"",
+      "courseMode":""
    }
-//    {
-//       "courseName":"",
-//       "courseimg":"",
-//       "courseMode":""
-//    },
    
    
 ]

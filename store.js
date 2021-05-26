0bebaf4d6ca0d145a0c023fdda6fbe3cbe883803
skdhameliya <53290891+skdhameliya@users.noreply.github.com>
2021-05-26 16:04:23 +0530

    Update store.js

--- store.js
+++ store.js
@@ -6,20 +6,151 @@
       "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1B2RHCflc0nf6j2Y4iQRprZ034PJrlHcM/view?usp=sharing"
    },
    {
-      "courseName":"Section 375",
-      "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fbolly%2F2019_section_375.jpg?alt=media&token=f7dbf2d1-e9f0-48d0-9617-9f04157f2613",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1B2RHCflc0nf6j2Y4iQRprZ034PJrlHcM/view?usp=sharing"
+      "courseName":"365 Days",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1nr97Kqn6yLS13cbfsq3jmYRmaZAPtQh_/view?usp=sharing"
    },
    {
-      "courseName":"Section 375",
-      "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fbolly%2F2019_section_375.jpg?alt=media&token=f7dbf2d1-e9f0-48d0-9617-9f04157f2613",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1B2RHCflc0nf6j2Y4iQRprZ034PJrlHcM/view?usp=sharing"
+      "courseName":"Hobbs & Shaw",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1BDtymLtpzGyFTHhRRYSx6WVJnXUcyjdv/view?usp=sharing"
    },
    {
-      "courseName":"Section 375",
-      "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fbolly%2F2019_section_375.jpg?alt=media&token=f7dbf2d1-e9f0-48d0-9617-9f04157f2613",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1B2RHCflc0nf6j2Y4iQRprZ034PJrlHcM/view?usp=sharing"
+      "courseName":"The Fate & The Furious",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1tLGseC_SscXz2qAzI3i0ULLd4t3_-UVW/view?usp=sharing"
+   },
+   {
+      "courseName":"Men In Black International",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1CsVqmWkM6c7BLsERO8IaOhdneatdi3kI/view?usp=sharing"
+   },
+   {
+      "courseName":"Love Ni Bhavai",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/12e68wzT368Us0GltpdnBmbfLLmDo5iil/view?usp=sharing"
+   },
+   {
+      "courseName":"Asur S1-E1",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/11c7qyBfFdePOqS_8C-0FM8Jl_NsiRc5V/view?usp=sharing"
+   },
+   {
+      "courseName":"Asur S1-E2",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1AK_74B3RnqLO3AeOonespGCKhQFaHPCJ/view?usp=sharing"
+   },
+   {
+      "courseName":"Asur S1-E3",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1sq7zu134tyoIHhdDKU8sFDPLAwFTgPoR/view?usp=sharing"
+   },
+   {
+      "courseName":"Asur S1-E4",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1WytxsmCldITsDncQruNcl00Eg3rNIUkJ/view?usp=sharing"
+   },
+   {
+      "courseName":"Asur S1-E5",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1K32V8BRIm3dfirOk8CqZ_pbwC4tQitk2/view?usp=sharing"
+   },
+   {
+      "courseName":"Asur S1-E7",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1kPUgSi_2oY8z_RpUE33gH57SDJh0veBW/view?usp=sharing"
+   },
+   {
+      "courseName":"Asur S1-E8",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1jxEdi38jkVv4ecEOoHakRg5hObHSM6U4/view?usp=sharing"
+   },
+   {
+      "courseName":"The Family Man S1-E1",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1d0y5sntOY96zxw3KnpzEQEYww1obMVE8/view?usp=sharing"
+   },
+   {
+      "courseName":"The Family Man S1-E2",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1_PYTrB0c6-3sCDAHq_vLTSAU3oRMluMn/view?usp=sharing"
+   },
+   {
+      "courseName":"The Family Man S1-E3",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/16Q-CCoRprp2cDpFNHMy4XbA0ecOEczeM/view?usp=sharing"
+   },
+   {
+      "courseName":"The Family Man S1-E4",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1la05HGnD8asvRK_Sb5GH-ovUo2gyUqlD/view?usp=sharing"
+   },
+   {
+      "courseName":"The Family Man S1-E5",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/11zzgIGyistNzSET1aWdH7ecODaBrVU8V/view?usp=sharing"
+   },
+   {
+      "courseName":"The Family Man S1-E6",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1OAwdDe6zagS5zveyoDp4O2HAOkqL3Lk3/view?usp=sharing"
+   },
+   {
+      "courseName":"The Family Man S1-E7",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1jkUc2SCIP8l1TalWwu3klYtOOyaoGWMP/view?usp=sharing"
+   },
+   {
+      "courseName":"The Family Man S1-E8",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1FkqmlDxSsNokJmHJ7qDicntZdcjYuzrE/view?usp=sharing"
+   },
+   {
+      "courseName":"The Family Man S1-E9",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1CPrRKPgoNkGraootTYtVoYylK5s8Hgc-/view?usp=sharing"
+   },
+   {
+      "courseName":"The Family Man S1-E10",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1AESiTnx2oMjyltIyZDWxKufqMdQQQDFR/view?usp=sharing"
+   },
+   {
+      "courseName":"Special Ops S1-E1-to-E4",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1CrOQO9KvqwP8HqF2Vga-7N1CaMKiITT3/view?usp=sharing"
+   },
+   {
+      "courseName":"Special Ops S1-E5-to-E8",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1YuppaFGIEnx3ElP_FyeqrLGG8PKuLCLl/view?usp=sharing"
+   },
+   {
+      "courseName":"Space Force S1-E1-to-E3",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/199Xrqf6WBHBEL631_u30CN841zbYxrtF/view?usp=sharing"
+   },
+   {
+      "courseName":"Space Force S1-E4-to-E7",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1rla0qvWck6Yaq_MOJSU2hd5X08noMF71/view?usp=sharing"
+   },
+   {
+      "courseName":"The Bull of Dalal Street S1-E1-to-E4",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1H_c470HwWvJjvTXmswf_gDBxmxBpa7O0/view?usp=sharing"
+   },
+   {
+      "courseName":"The Bull of Dalal Street S1-E5-to-E8",
+      "courseimg":"",
+      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/10iFKjnSDSoEI2hrsZf0rDyV2JuiqDpLk/view?usp=sharing"
    }
+   
+//    {
+//       "courseName":"",
+//       "courseimg":"",
+//       "courseMode":""
+//    },
    
    
 ]

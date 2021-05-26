5de2c2ff9d5d665962a6b53fe308ff25aea4698d
skdhameliya <53290891+skdhameliya@users.noreply.github.com>
2021-05-26 15:38:36 +0530

    Update store.js

--- store.js
+++ store.js
@@ -1,34 +1,24 @@
 [
    
    {
-      "courseName":"",
-      "courseimg":"",
-      "courseMode":""
+      "courseName":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1B2RHCflc0nf6j2Y4iQRprZ034PJrlHcM/view?usp=sharing",
+      "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fbolly%2F2019_section_375.jpg?alt=media&token=f7dbf2d1-e9f0-48d0-9617-9f04157f2613",
+      "courseMode":"Section 375"
    },
    {
-      "courseName":"",
-      "courseimg":"",
-      "courseMode":""
+      "courseName":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1B2RHCflc0nf6j2Y4iQRprZ034PJrlHcM/view?usp=sharing",
+      "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fbolly%2F2019_section_375.jpg?alt=media&token=f7dbf2d1-e9f0-48d0-9617-9f04157f2613",
+      "courseMode":"Section 375"
    },
    {
-      "courseName":"",
-      "courseimg":"",
-      "courseMode":""
+      "courseName":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1B2RHCflc0nf6j2Y4iQRprZ034PJrlHcM/view?usp=sharing",
+      "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fbolly%2F2019_section_375.jpg?alt=media&token=f7dbf2d1-e9f0-48d0-9617-9f04157f2613",
+      "courseMode":"Section 375"
    },
    {
-      "courseName":"",
-      "courseimg":"",
-      "courseMode":""
-   },
-   {
-      "courseName":"",
-      "courseimg":"",
-      "courseMode":""
-   },
-   {
-      "courseName":"",
-      "courseimg":"",
-      "courseMode":""
+      "courseName":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1B2RHCflc0nf6j2Y4iQRprZ034PJrlHcM/view?usp=sharing",
+      "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fbolly%2F2019_section_375.jpg?alt=media&token=f7dbf2d1-e9f0-48d0-9617-9f04157f2613",
+      "courseMode":"Section 375"
    }
    
    

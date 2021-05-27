8e7c567d88eb7c7c85f50bd2f37c55fe75598708
skdhameliya <53290891+skdhameliya@users.noreply.github.com>
2021-05-27 13:11:58 +0530

    Update store.js

--- store.js
+++ store.js
@@ -3,147 +3,147 @@
    {
       "courseName":"Section 375",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fbolly%2F2019_section_375.jpg?alt=media&token=f7dbf2d1-e9f0-48d0-9617-9f04157f2613",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1B2RHCflc0nf6j2Y4iQRprZ034PJrlHcM/view?usp=sharing"
+      "courseMode":"https://xpshort.com/section-375"
    },
    {
       "courseName":"Hobbs & Shaw",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fholly_hin%2F2019_Hobbs_%26_Shaw.jpg?alt=media&token=f36abf44-7f99-4546-a04d-fc3266ca56f8",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1BDtymLtpzGyFTHhRRYSx6WVJnXUcyjdv/view?usp=sharing"
+      "courseMode":"https://xpshort.com/Hobbs-shaw"
    },
    {
       "courseName":"The Fate & The Furious 8",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fholly_hin%2F2017_the_fate_of_the_furious.jpg?alt=media&token=754b4958-f9ee-4140-a4c7-9b6d89b2551c",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1tLGseC_SscXz2qAzI3i0ULLd4t3_-UVW/view?usp=sharing"
+      "courseMode":"https://xpshort.com/The-fate-furious-8"
    },
    {
       "courseName":"Men In Black International",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fholly_hin%2F2019_men_in_black_international.jpg?alt=media&token=4f8d1551-574d-4234-a212-279da8581eb0",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1CsVqmWkM6c7BLsERO8IaOhdneatdi3kI/view?usp=sharing"
+      "courseMode":"https://xpshort.com/mib-international"
    },
    {
       "courseName":"Love Ni Bhavai",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fguj%2F2017_love_ni_bhavai.jpg?alt=media&token=0fd489d5-f555-40f7-bcc4-cbc3f6544f40",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/12e68wzT368Us0GltpdnBmbfLLmDo5iil/view?usp=sharing"
+      "courseMode":"https://xpshort.com/love-ni-bhavai"
    },
    {
       "courseName":"Asur S1-E1",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fasur%2F2020_Asur.jpg?alt=media&token=c6c656ac-d583-4b23-913c-7135c7c17111",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/11c7qyBfFdePOqS_8C-0FM8Jl_NsiRc5V/view?usp=sharing"
+      "courseMode":"https://xpshort.com/asur-s1e1"
    },
    {
       "courseName":"Asur S1-E2",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fasur%2F2020_Asur.jpg?alt=media&token=c6c656ac-d583-4b23-913c-7135c7c17111",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1AK_74B3RnqLO3AeOonespGCKhQFaHPCJ/view?usp=sharing"
+      "courseMode":"https://xpshort.com/asur-s1e2"
    },
    {
       "courseName":"Asur S1-E3",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fasur%2F2020_Asur.jpg?alt=media&token=c6c656ac-d583-4b23-913c-7135c7c17111",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1sq7zu134tyoIHhdDKU8sFDPLAwFTgPoR/view?usp=sharing"
+      "courseMode":"https://xpshort.com/asur-s1e3"
    },
    {
       "courseName":"Asur S1-E4",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fasur%2F2020_Asur.jpg?alt=media&token=c6c656ac-d583-4b23-913c-7135c7c17111",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1WytxsmCldITsDncQruNcl00Eg3rNIUkJ/view?usp=sharing"
+      "courseMode":"https://xpshort.com/asur-s1e4"
    },
    {
       "courseName":"Asur S1-E5",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fasur%2F2020_Asur.jpg?alt=media&token=c6c656ac-d583-4b23-913c-7135c7c17111",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1K32V8BRIm3dfirOk8CqZ_pbwC4tQitk2/view?usp=sharing"
+      "courseMode":"https://xpshort.com/asur-s1e5"
    },
    {
       "courseName":"Asur S1-E7",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fasur%2F2020_Asur.jpg?alt=media&token=c6c656ac-d583-4b23-913c-7135c7c17111",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1kPUgSi_2oY8z_RpUE33gH57SDJh0veBW/view?usp=sharing"
+      "courseMode":"https://xpshort.com/asur-s1e7"
    },
    {
       "courseName":"Asur S1-E8",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fasur%2F2020_Asur.jpg?alt=media&token=c6c656ac-d583-4b23-913c-7135c7c17111",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1jxEdi38jkVv4ecEOoHakRg5hObHSM6U4/view?usp=sharing"
+      "courseMode":"https://xpshort.com/asur-s1e8"
    },
    {
       "courseName":"365 Days",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/mv%2Fholly_hin%2F2020_365_days.jpg?alt=media&token=9d365adf-c046-4e37-808f-8359a3d49e83",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1nr97Kqn6yLS13cbfsq3jmYRmaZAPtQh_/view?usp=sharing"
+      "courseMode":"https://xpshort.com/365-days"
    },
    {
       "courseName":"The Family Man S1-E1",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Ffamily%20man%20s1%2F2020_The_Family_Man.jpg?alt=media&token=8f834f77-874d-4709-9f22-6c8d58cfb5d7",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1d0y5sntOY96zxw3KnpzEQEYww1obMVE8/view?usp=sharing"
+      "courseMode":"https://xpshort.com/family-man-s1e1"
    },
    {
       "courseName":"The Family Man S1-E2",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Ffamily%20man%20s1%2F2020_The_Family_Man.jpg?alt=media&token=8f834f77-874d-4709-9f22-6c8d58cfb5d7",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1_PYTrB0c6-3sCDAHq_vLTSAU3oRMluMn/view?usp=sharing"
+      "courseMode":"https://xpshort.com/family-man-s1e2"
    },
    {
       "courseName":"The Family Man S1-E3",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Ffamily%20man%20s1%2F2020_The_Family_Man.jpg?alt=media&token=8f834f77-874d-4709-9f22-6c8d58cfb5d7",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/16Q-CCoRprp2cDpFNHMy4XbA0ecOEczeM/view?usp=sharing"
+      "courseMode":"https://xpshort.com/family-man-s1e3"
    },
    {
       "courseName":"The Family Man S1-E4",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Ffamily%20man%20s1%2F2020_The_Family_Man.jpg?alt=media&token=8f834f77-874d-4709-9f22-6c8d58cfb5d7",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1la05HGnD8asvRK_Sb5GH-ovUo2gyUqlD/view?usp=sharing"
+      "courseMode":"https://xpshort.com/family-man-s1-e4"
    },
    {
       "courseName":"The Family Man S1-E5",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Ffamily%20man%20s1%2F2020_The_Family_Man.jpg?alt=media&token=8f834f77-874d-4709-9f22-6c8d58cfb5d7",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/11zzgIGyistNzSET1aWdH7ecODaBrVU8V/view?usp=sharing"
+      "courseMode":"https://xpshort.com/family-man-s1-e5"
    },
    {
       "courseName":"The Family Man S1-E6",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Ffamily%20man%20s1%2F2020_The_Family_Man.jpg?alt=media&token=8f834f77-874d-4709-9f22-6c8d58cfb5d7",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1OAwdDe6zagS5zveyoDp4O2HAOkqL3Lk3/view?usp=sharing"
+      "courseMode":"https://xpshort.com/family-man-s1-e6"
    },
    {
       "courseName":"The Family Man S1-E7",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Ffamily%20man%20s1%2F2020_The_Family_Man.jpg?alt=media&token=8f834f77-874d-4709-9f22-6c8d58cfb5d7",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1jkUc2SCIP8l1TalWwu3klYtOOyaoGWMP/view?usp=sharing"
+      "courseMode":"https://xpshort.com/family-man-s1-e7"
    },
    {
       "courseName":"The Family Man S1-E8",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Ffamily%20man%20s1%2F2020_The_Family_Man.jpg?alt=media&token=8f834f77-874d-4709-9f22-6c8d58cfb5d7",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1FkqmlDxSsNokJmHJ7qDicntZdcjYuzrE/view?usp=sharing"
+      "courseMode":"https://xpshort.com/family-man-s1-e8"
    },
    {
       "courseName":"The Family Man S1-E9",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Ffamily%20man%20s1%2F2020_The_Family_Man.jpg?alt=media&token=8f834f77-874d-4709-9f22-6c8d58cfb5d7",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1CPrRKPgoNkGraootTYtVoYylK5s8Hgc-/view?usp=sharing"
+      "courseMode":"https://xpshort.com/family-man-s1-e9"
    },
    {
       "courseName":"The Family Man S1-E10",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Ffamily%20man%20s1%2F2020_The_Family_Man.jpg?alt=media&token=8f834f77-874d-4709-9f22-6c8d58cfb5d7",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1AESiTnx2oMjyltIyZDWxKufqMdQQQDFR/view?usp=sharing"
+      "courseMode":"https://xpshort.com/family-man-s1-e10"
    },
    {
       "courseName":"Special Ops S1-E1-to-E4",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fspecial%20ops%2F2020_special_ops.jpg?alt=media&token=cbe63753-7dad-4592-add0-e025aaae67f9",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1CrOQO9KvqwP8HqF2Vga-7N1CaMKiITT3/view?usp=sharing"
+      "courseMode":"https://xpshort.com/special-ops-s1e1-e4"
    },
    {
       "courseName":"Special Ops S1-E5-to-E8",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fspecial%20ops%2F2020_special_ops.jpg?alt=media&token=cbe63753-7dad-4592-add0-e025aaae67f9",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1YuppaFGIEnx3ElP_FyeqrLGG8PKuLCLl/view?usp=sharing"
+      "courseMode":"https://xpshort.com/special-ops-s1e5-e8"
    },
    {
       "courseName":"Space Force S1-E1-to-E3",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fspecial%20force%2F2020_space_force.jpg?alt=media&token=7ce54d25-10f1-4d1f-8759-a4858147f490",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/199Xrqf6WBHBEL631_u30CN841zbYxrtF/view?usp=sharing"
+      "courseMode":"https://xpshort.com/special-force-s1e1-e3"
    },
    {
       "courseName":"Space Force S1-E4-to-E7",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fspecial%20force%2F2020_space_force.jpg?alt=media&token=7ce54d25-10f1-4d1f-8759-a4858147f490",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1rla0qvWck6Yaq_MOJSU2hd5X08noMF71/view?usp=sharing"
+      "courseMode":"https://xpshort.com/special-force-s1e4-e7"
    },
    {
       "courseName":"The Bull of Dalal Street S1-E1-to-E4",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fthe%20bull%20of%20dalal%20street%2F2020_The_Bull_of_Dalal_Street.jpg?alt=media&token=eac55bf1-283c-4818-9cb0-5acea7c50697",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/1H_c470HwWvJjvTXmswf_gDBxmxBpa7O0/view?usp=sharing"
+      "courseMode":"https://xpshort.com/bull-dalal-street-s1e1-e4"
    },
    {
       "courseName":"The Bull of Dalal Street S1-E5-to-E8",
       "courseimg":"https://firebasestorage.googleapis.com/v0/b/f3movie.appspot.com/o/ws%2Fthe%20bull%20of%20dalal%20street%2F2020_The_Bull_of_Dalal_Street.jpg?alt=media&token=eac55bf1-283c-4818-9cb0-5acea7c50697",
-      "courseMode":"https://ushort7.com/st?api=dfacd6797bf987f460773ec8eadd380fd9f0dd3f&url=https://drive.google.com/file/d/10iFKjnSDSoEI2hrsZf0rDyV2JuiqDpLk/view?usp=sharing"
+      "courseMode":"https://xpshort.com/bull-dalal-street-s1e5-e8"
    }
    
 //    {
